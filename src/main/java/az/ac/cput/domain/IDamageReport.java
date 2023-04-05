5c865aa2656177fcc065fff65c491c14b99069ef
Cwenga Dlova <cwenga.dlova.19@gmail.com>
2023-04-05 23:54:29 +0200

    DamageReport and Reservation builder classes, as well as their test classes.

---
+++ src/main/java/az/ac/cput/domain/IDamageReport.java
@@ -0,0 +1,16 @@
+package az.ac.cput.domain;
+
+import az.ac.cput.scratch.Customer;
+import az.ac.cput.scratch.Rental;
+
+import java.time.LocalDate;
+
+public interface IDamageReport {
+    String getId();
+    Rental getRentalId();
+    Customer getCustomerId();
+    Car getCarId();
+    LocalDate getReportDate();
+    String getDamageLocation();
+    String getDescription();
+}
