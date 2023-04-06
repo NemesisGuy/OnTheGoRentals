f2e4e67e350880a80ccf12dcf5c9c56249dc2036
Reign Buckingham <24620376+NemesisGuy@users.noreply.github.com>
2023-04-06 16:46:59 +0200

    Added some endpoints

---
+++ src/main/java/za/ac/cput/repository/IPaymentRepository.java
@@ -0,0 +1,11 @@
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.Payment;
+
+import java.util.List;
+
+public interface IPaymentRepository extends IRepository<Payment, Integer>{
+    List<Payment> getAllPayments();
+    Payment getPaymentById(Integer id);
+
+}
