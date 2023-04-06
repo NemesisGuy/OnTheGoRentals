fc4174ac934f4955b4b97315c03bb5966301b552
Reign Buckingham <24620376+NemesisGuy@users.noreply.github.com>
2023-04-06 18:56:42 +0200

    recovered files -1

---
+++ src/test/java/za/ac/cput/factory/PaymentFactoryTest.java
@@ -0,0 +1,28 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Payment;
+import java.time.LocalDate;
+
+class PaymentFactoryTest {
+
+    @Test
+    void testPaymentFactory_pass() {
+        PaymentFactory paymentFactory = new PaymentFactory();
+        Payment payment = paymentFactory.create();
+
+        Assertions.assertNotNull(payment);
+        Assertions.assertNotNull(payment.getPaymentId());
+    }
+
+    @Test
+    void testPaymentFactory_fail() {
+        PaymentFactory paymentFactory = new PaymentFactory();
+        Payment payment = paymentFactory.create();
+
+        Assertions.assertNull(payment);
+        Assertions.assertNull(payment.getPaymentId());
+    }
+
+}
