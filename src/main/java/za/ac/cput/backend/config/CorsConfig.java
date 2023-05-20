9a8be31fd6145a05dfcf91f9f59c95ceb56df4aa
Reign Buckingham <24620376+NemesisGuy@users.noreply.github.com>
2023-05-20 10:23:45 +0200

    added cors and security config,login and register endpoints ect..

---
+++ src/main/java/za/ac/cput/backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package za.ac.cput.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("NullableProblems")
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
