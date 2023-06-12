e124c29fdcd82bb2f992334f8d1742eb2869e6dd
Cwenga Dlova <cwenga.dlova.19@gmail.com>
2023-06-12 21:48:27 +0200

    Ammended factory class. Deleted damage report interface in the domain and ammended it's class.

--- src/main/java/za/ac/cput/factory/impl/DamageReportFactory.java
+++ src/main/java/za/ac/cput/factory/impl/DamageReportFactory.java
@@ -5,11 +5,13 @@ package za.ac.cput.factory.impl;
  * Author: Cwenga Dlova (214310671)
  * Date:  06 April 2023
  */
+import za.ac.cput.domain.impl.Car;
+import za.ac.cput.domain.impl.Customer;
 import za.ac.cput.domain.impl.DamageReport;
+import za.ac.cput.domain.impl.Rental;
 import za.ac.cput.factory.IFactory;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Random;
 
 public class DamageReportFactory implements IFactory<DamageReport> {
@@ -18,39 +20,26 @@ public class DamageReportFactory implements IFactory<DamageReport> {
     public DamageReport create() {
         return new DamageReport.Builder()
                 .setId(new Random().nextInt(1000000))
+                .setRentalId(null)
+                .setCustomerId(null)
+                .setCarId(new Car())
                 .setReportDate(LocalDate.parse("2023-01-05"))
                 .setDamageLocation("Cape Town")
                 .setDescription("Drunk driving recklessness.")
                 .build();
     }
 
+    public DamageReport createDamageReport(Rental rentalId, Customer customerId, LocalDate reportDate, String damageLocation, String description) {
 
-    public DamageReport getById(long id) {
-        return null;
-    }
+        return new DamageReport.Builder()
+                .setId(new Random().nextInt())
+                .setRentalId(rentalId)
+                .setCustomerId(customerId)
+                .setCarId(Car.builder().build())
+                .setReportDate(reportDate)
+                .setDamageLocation(damageLocation)
+                .setDescription(description)
+                .build();
 
-
-    public DamageReport update(DamageReport entity) {
-        return null;
-    }
-
-
-    public boolean delete(DamageReport entity) {
-        return false;
-    }
-
-
-    public List<DamageReport> getAll() {
-        return null;
-    }
-
-
-    public long count() {
-        return 0;
-    }
-
-
-    public Class<DamageReport> getType() {
-        return null;
     }
 }
