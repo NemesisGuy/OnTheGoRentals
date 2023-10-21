8e0a5d736fbdb844dbc2a0afb111711b58fd3c65
Peter Buckingham <your.email@example.com>
2023-10-21 11:10:25 +0200

    locked down admin endpoints in one line

--- src/main/java/za/ac/cput/security/SpringSecurityConfig.java
+++ src/main/java/za/ac/cput/security/SpringSecurityConfig.java
@@ -44,6 +44,7 @@ public class SpringSecurityConfig {
             .requestMatchers("/api/settings/read").permitAll()
 
             //user car endpoints
+
             .requestMatchers("/api/cars/list/*").permitAll()
             .requestMatchers("/api/cars/list/available/*").permitAll()
 
@@ -54,25 +55,27 @@ public class SpringSecurityConfig {
             .requestMatchers("/api/admin/settings/update").hasAuthority("ADMIN")
 
             //admin cars endpoints
-            .requestMatchers("/api/admin/cars/all").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/cars/create").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/cars/read/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/cars/update/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/cars/delete/*").hasAuthority("ADMIN")
-            //admin users endpoints
-            .requestMatchers("/api/admin/users/list/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/users/create").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/users/read/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/users/update/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/users/update/*/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/users/delete/*").hasAuthority("ADMIN")
-            //admin rentals endpoints
-            .requestMatchers("/api/admin/rentals/all").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/rentals/list/all").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/rentals/create").hasAuthority("ADMIN")//issues there on front end
-            .requestMatchers("/api/admin/rentals/read/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/rentals/update/*").hasAuthority("ADMIN")
-            .requestMatchers("/api/admin/rentals/delete/*").hasAuthority("ADMIN")
+
+            .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/cars/all").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/cars/create").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/cars/read/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/cars/update/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/cars/delete/*").hasAuthority("ADMIN")
+//            //admin users endpoints
+//            .requestMatchers("/api/admin/users/list/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/users/create").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/users/read/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/users/update/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/users/update/*/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/users/delete/*").hasAuthority("ADMIN")
+//            //admin rentals endpoints
+//            .requestMatchers("/api/admin/rentals/all").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/rentals/list/all").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/rentals/create").hasAuthority("ADMIN")//issues there on front end
+//            .requestMatchers("/api/admin/rentals/read/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/rentals/update/*").hasAuthority("ADMIN")
+//            .requestMatchers("/api/admin/rentals/delete/*").hasAuthority("ADMIN")
             //admin bookings endpoints
 
             //admin faqs endpoints
