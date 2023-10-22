de643c0967fb8eef1b05799b2c4c9271b0130c98
Shamiso <shamiso@databalk.nu>
2023-10-22 22:29:56 +0200

    Feedback Backend

--- src/main/java/za/ac/cput/config/CorsConfig.java
+++ src/main/java/za/ac/cput/config/CorsConfig.java
@@ -6,32 +6,45 @@ package za.ac.cput.config;
  * Date: 05 April 2023
  */
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
             @SuppressWarnings("NullableProblems")
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        /*.allowedOrigins("http://localhost:5173")*/
-                        .allowedOrigins("http://localhost:5173", "http://192.168.0.105:5173", "http://192.168.56.1:5173", "http://172.31.112.1:5173", "http://192.0.0.0/8:5173", "http://192.169.0.101:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        // .allowedOrigins(allowedOrigins) //user this for when locking down the allowed origins
+                        .allowedOriginPatterns(allowedOrigins) //use this for when allowing all origins
+                        .allowedMethods("*")
+                        .allowedOrigins("*")
                         .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .exposedHeaders("Access-Control-Allow-Origin")
+                        // .allowCredentials(true)
+                        .exposedHeaders("*")
                         .maxAge(3600);
                 System.out.println("CorsConfig was triggered");
 
             }
         };
     }
+
 }
+
+
+
